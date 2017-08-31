9acc797566e3eb9e35c1309e6c2b413c9d1a9387
Jeison J <jeisonj_2008@hotmail.com>
2017-08-31 07:18:28 -0400

    Add files via upload

--- caracteresAleatorios.c
+++ caracteresAleatorios.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h> // Incluye srand() y rand().
+#include <time.h>   // Incluye time().
 
 char matriz[2][7];
 char letras[] = "ABCDEFGHIJKLMN";
 
 int main() {
 
-	int contador = 0;
+	int contador = 1;
 	
+	srand(time(0));
 	for (int i = 0; i < 2; i++) {
 		for (int j = 0; j < 7; j++) {
-			matriz[i][j] = letras[contador];
+			matriz[i][j] = letras[rand()%contador];
 			printf("%c \t", matriz[i][j]);
 			contador++;
 		}
